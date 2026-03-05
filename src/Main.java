6b79dfbb5b078723a56095cf9f979fe177009b24
lleekh0920 <lleekh0920@gmail.com>
2026-03-05 12:38:56 +0900

    이슈해결

--- src/Main.java
+++ src/Main.java
@@ -19,8 +19,7 @@ public class Main {
         //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
         System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
+        //test가 너무 많이 출력돼서 지움
 
 
         for (int i = 1; i <= 5; i++) {
