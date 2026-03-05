7f49cce86995445c7031aadb689cdcbdb9b06f98
lleekh0920 <lleekh0920@gmail.com>
2026-03-05 12:47:30 +0900

    성적 추가

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,19 @@ public class Main {
         System.out.println("기능 b 추가 구현");
     }
 
-
     public static void main(String[] args) {
 
         //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
         System.out.println("test");
         //test가 너무 많이 출력돼서 지움
+        int score=77;
+        String grade=switch (score/10){
+
+            case 7->"c";
+
+            default -> throw new IllegalStateException("Unexpected value: " + score);
+        };
 
 
         for (int i = 1; i <= 5; i++) {
