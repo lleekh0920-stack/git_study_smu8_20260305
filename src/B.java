6da9d99378d63e3e9745a6b12f1d8300c765d2e4
lleekh0920 <lleekh0920@gmail.com>
2026-03-06 11:05:22 +0900

    생성자 함수 복습

--- src/B.java
+++ src/B.java
@@ -1,5 +1,26 @@
 public class B {
-    public static void main(String[] args) {
-        System.out.println("한번 더 복습");
+    int a;
+    int b;
+    public B(){}
+    public int sum(){
+        return a+b;
     }
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("생성자,합수 복습");
+        new B();
+        new B().sum();
+        int sum=new B().sum();
+        System.out.println(sum);
+
+        B b=new B();
+
+
+
+
+        }
 }
